9605049e05768008f8af9a9514f81a8d959beaf5
sq <2287247953@qq.com>
2020-04-22 20:56:47 +0800

    表单登录对象,第20个视频

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,7 @@
+import { Button, Form, FormItem, Input } from 'element-ui'
 import Vue from 'vue'
-import { Button } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
