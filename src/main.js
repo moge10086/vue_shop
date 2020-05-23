e89c9e71291a6473a052413689a5efecdf318726
sq <2287247953@qq.com>
2020-05-23 17:56:50 +0800

    good_list

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
 import _ from 'lodash'
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 Vue.config.devtools = true
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -17,6 +22,21 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 Vue.prototype._ = _
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   render: h => h(App)
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from '../components/power/Rights.vue'
 import Role from '../components/power/Role.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
+import GoodsList from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
 Vue.use(VueRouter)
 
 const routes = [{
@@ -44,6 +46,13 @@ const routes = [{
   }, {
     path: '/params',
     component: Params
+  },
+  {
+    path: '/goods',
+    component: GoodsList
+  }, {
+    path: '/goods/add',
+    component: Add
   }
   ]
 }
